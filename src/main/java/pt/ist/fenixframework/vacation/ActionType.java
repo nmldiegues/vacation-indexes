e9c28a498443b4fc1a914887b66e032090a7aa28
Sanne Grinovero <sanne@hibernate.org>
2013-08-09 00:54:59 +0100

    Use enums to encode specific behaviour of each ActionType

--- src/main/java/pt/ist/fenixframework/vacation/ActionType.java
+++ src/main/java/pt/ist/fenixframework/vacation/ActionType.java
@@ -1,11 +1,30 @@
 package pt.ist.fenixframework.vacation;
 
+import pt.ist.fenixframework.vacation.domain.VacationManager;
+
 public enum ActionType {
 	
-	ACTION_MAKE_RESERVATION,
+	ACTION_MAKE_RESERVATION {
+		@Override
+		public Operation createOperation(VacationManager managerPtr, Random randomPtr, int numQueryPerTransaction, int queryRange, boolean readOnly) {
+			return new MakeReservationOperation(managerPtr, randomPtr, numQueryPerTransaction, queryRange, readOnly);
+		}
+	},
 	
-	ACTION_DELETE_CUSTOMER,
+	ACTION_DELETE_CUSTOMER {
+		@Override
+		public Operation createOperation(VacationManager managerPtr, Random randomPtr, int numQueryPerTransaction, int queryRange, boolean readOnly) {
+			return new DeleteCustomerOperation(managerPtr, randomPtr, queryRange);
+		}
+	},
 	
-	ACTION_UPDATE_TABLES,
+	ACTION_UPDATE_TABLES {
+		@Override
+		public Operation createOperation(VacationManager managerPtr, Random randomPtr, int numQueryPerTransaction, int queryRange, boolean readOnly) {
+			return new UpdateTablesOperation(managerPtr, randomPtr, numQueryPerTransaction, queryRange);
+		}
+	};
+
+	public abstract Operation createOperation(VacationManager managerPtr, Random randomPtr, int numQueryPerTransaction, int queryRange, boolean readOnly);
 
 }
